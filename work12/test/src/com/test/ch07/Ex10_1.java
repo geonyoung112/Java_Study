f9b6343f4540b743514db752e2b20c53ab654a17
geonyoung112 <guns3176@naver.com>
2023-01-10 10:21:24 +0900

    today
    today_study

--- work12/test/src/com/test/ch07/Ex10_1.java
+++ work12/test/src/com/test/ch07/Ex10_1.java
@@ -4,13 +4,13 @@ public class Ex10_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Movie c1 = new Movie("어벤져스");
+		Movie c1 = new Movie("�뼱踰ㅼ졇�뒪");
 		Movie.printInfo(c1);
 		
-		Movie c2 = new Movie("연애의 온도",180);
+		Movie c2 = new Movie("�뿰�븷�쓽 �삩�룄",180);
 		Movie.printInfo(c2);
 		
-		Movie c3 = new Movie("아바타",180, "판타지");
+		Movie c3 = new Movie("�븘諛뷀�",180, "�뙋��吏�");
 		Movie.printInfo(c3);
 	}
 
@@ -22,11 +22,11 @@ class Movie {
 	String gerne;
 	
 	Movie (String title) {
-		this(title, 100, "액션");
+		this(title, 100, "�븸�뀡");
 	}
 	
 	Movie (String title, int hour) {
-		this(title, hour, "로맨스");
+		this(title, hour, "濡쒕㎤�뒪");
 	}
 	
 	Movie (String title, int hour, String gerne) {
@@ -38,10 +38,10 @@ class Movie {
 	
 	
 	static void printInfo(Movie c) {
-		System.out.printf("제목: %s%n상영시간: %d%n장르: %s%n%n", c.title, c.hour, c.gerne);
+		System.out.printf("�젣紐�: %s%n�긽�쁺�떆媛�: %d%n�옣瑜�: %s%n%n", c.title, c.hour, c.gerne);
 	}
 }
 /*
-문제 1.
-이전 문제에서 작성한 영화 클래스의 생성자를 this를 사용하도록 수정하세요. 
+臾몄젣 1.
+�씠�쟾 臾몄젣�뿉�꽌 �옉�꽦�븳 �쁺�솕 �겢�옒�뒪�쓽 �깮�꽦�옄瑜� this瑜� �궗�슜�븯�룄濡� �닔�젙�븯�꽭�슂. 
 */

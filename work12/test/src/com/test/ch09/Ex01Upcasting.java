1c66608732bc4ce71e6539a5fed896fda505a81b
geonyoung112 <guns3176@naver.com>
2023-01-18 10:44:44 +0900

    up, down casting study
    2023/01/18

--- work12/test/src/com/test/ch09/Ex01Upcasting.java
+++ work12/test/src/com/test/ch09/Ex01Upcasting.java
@@ -9,11 +9,16 @@ public class Ex01Upcasting {
 		book.setTitle("자바 기본문법");
 		System.out.println(book.getTitle());
 		
-		book = new EBook();
-		//book.setFontColor("블랙");
-		//System.out.println(book.getFontColor);
+		book = new EBook(); //업	캐스팅
+		book.setFontColor("블랙");
+		System.out.println(book.getFontColor());
 		book.setTitle("자바 자료구조");
 		System.out.println(book.getTitle());
+		
+		book = new AudioBook2(); //업 캐스팅
+		//book.setTitle("파이썬 자료구조");
+		//System.out.println(book.getTitle());
+		book.printVol(50);
 	}
 
 }
@@ -27,6 +32,18 @@ class Book {
 	
 	public void setTitle(String title) {
 		this.title = title;
+	}
+	
+	public String getFontColor() {
+		return "";
+	}
+	
+	public void setFontColor(String fontColor) {
+		
+	}
+	
+	public void printVol(int volume) {
+
 	}
 }
 
@@ -43,15 +60,49 @@ class EBook extends Book {
 	
 	@Override
 	public void setTitle(String title) {
-		title = "[EBOOK]" + title;
+		title = "[EBOOK] " + title;
+		super.setTitle(title);
+	}
+}
+
+class AudioBook2 extends Book {
+	private String fontColor;
+	private int volume;
+	
+	public void printVol(int volume) {
+		this.volume = volume;
+		System.out.println("볼륨은 : " + this.volume);
+	}
+	
+	public String getFontColor() {
+		return fontColor;
+	}
+	
+	public void setFontColor(String fontColor) {
+		this.fontColor = fontColor;
+	}
+	
+	@Override
+	public void setTitle(String title) {
+		title = "[AUDIO BOOK] " + title;
 		super.setTitle(title);
 	}
 }
 
 /*
-문제 1.
+ * 업캐스팅이란?
+ * 부모는 자식을 모두 받을 수 있다.
+ * 부모> 자식 : 자식의 크기가 더 커서 착각할 수 있어도 부모는 자식을 다 받을 수 있는 것은 공식
+ * 업캐스팅을 이용해 자식의 메소드를 불러올 수 있다.
+ * 이는 다형성, 부모를 지역변수로 가져와 사용한다면 이는 부모의 자식도 각각 사용할 수 있다.
+ * 하지만 주의점은 부모에 없는 자식의 메소드는 사용할 수 없다.
+ * 그래서 내용을 제거하더라도 부모에 자식 메소드의 정의라도 해야지 사용할 수 있다.
+ */
+
+/*
+문제 1. o
 book.setFontColor("블랙") 코드에서 컴파일 오류가 나지 않도록 수정해 주세요.
-문제 2.
+문제 2. o
 Book의 자식 클래스인 AudioBook 클래스를 추가해 주세요.
 업캐스팅을 이용해 AudioBook 클래스의 특정 메소드를 호출하는 코드를 작성하세요. 
 */
--- work12/test/src/com/test/ch09/Ex02Polymorphism.java
+++ work12/test/src/com/test/ch09/Ex02Polymorphism.java
@@ -6,17 +6,24 @@ public class Ex02Polymorphism {
 		// TODO Auto-generated method stub
 		Book2 book = new Book2();
 		book.setTitle("자바 기본문법");
+
 		
 		EBook2 ebook = new EBook2();
 		ebook.setTitle("자바 알고리즘");
+		ebook.setFontColor("블루");
 		
 		ComicBook cbook = new ComicBook();
 		cbook.setTitle("만화로 배우는 자바");
+		
+		AudioBook abook = new AudioBook();
+		abook.setTitle("오디오로 배우는 자바");
+
 		
 		Cart cart  = new Cart();
 		cart.add(book);
 		cart.add(ebook);
 		cart.add(cbook);
+		cart.add(abook);
 		
 		cart.printList();
 		
@@ -35,6 +42,15 @@ class Book2 {
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
+	public String getFontColor() {
+		return "";
+	}
+	
+	public void setFontColor(String fontColor) {
+		
+	}
+
 }
 
 
@@ -42,7 +58,7 @@ class EBook2 extends Book2 {
 	private String fontColor;
 	
 	public String getFontColor() {
-		return fontColor;
+		return "이 책의 폰트 색은 " + fontColor + "입니다.";
 	}
 	
 	public void setFontColor(String fontColor) {
@@ -72,6 +88,16 @@ class ComicBook extends Book2 {
 	}
 }
 
+class AudioBook extends Book2 {
+	
+	@Override
+	public String getTitle() {
+		return "[오디오북]" + title;
+	}
+	//오버라이딩은 다형성을 많이 도와줌
+}
+
+
 class Cart {
 	private Book2[] books = new Book2[10];
 	private int index = 0;
@@ -86,15 +112,17 @@ class Cart {
 			if(books[i] == null)
 				break;
 			System.out.println(books[i].getTitle());
-			// (1) System.out.println(books[i].getFontColor());
+			System.out.println((books[i]).getFontColor());
+			System.out.println();
 		}
 	}
 }
 
+
 /*
-문제 1.
+문제 1. ?
 (1) 컴파일 에러가 발생하지 않도록 수정하세요.
-문제 2.
+문제 2. o
 AudioBook 클래스를 추가하세요.
 카트에 오디오북을 담으세요.
 카트의 리스트 출력 시 오디오북은 [오디오북]책제목의 형식으로 출력되도록 코딩하세요. 

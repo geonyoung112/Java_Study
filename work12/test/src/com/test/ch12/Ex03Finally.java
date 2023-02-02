ef619631d3af9c4dc4e38c5eb09a253c939c4692
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-02 09:31:08 +0900

    Finally study
    2023.02.02

---
+++ work12/test/src/com/test/ch12/Ex03Finally.java
@@ -0,0 +1,32 @@
+package com.test.ch12;
+
+import java.util.Scanner;
+
+public class Ex03Finally {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean run = true;
+		
+		Scanner scanner = new Scanner(System.in);
+		int num = 0;
+		
+		try {
+			System.out.print("문자열을 입력하여 예외를 발생시키세요: ");
+			num = scanner.nextInt();
+			
+		} catch(Exception e) {
+			System.out.println("숫자만 입력하세요.");
+			
+		} finally {
+			System.out.println("예외의 발생 여부와 상관없이 실행됩니다.");
+			
+		}
+	}
+
+}
+/*
+문제 1.
+예외의 여부와 상관없이 num의 값이 출력되도록 코딩하세요.
+*/

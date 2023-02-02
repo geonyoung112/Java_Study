341a9a0c0688b3ec00900f893c958ef3df434a98
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-02 11:44:58 +0900

    create throw file study
    2023.02.02

---
+++ work12/test/src/com/test/ch12/Ex05Throw.java
@@ -0,0 +1,45 @@
+package com.test.ch12;
+
+public class Ex05Throw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 while(true) {
+			User user = null;
+			
+			user = LoginService,form();
+			LoginService.login(user);
+			
+			if(user.isLogin()) {
+				LoginService.success(user);
+				break;
+			}
+			System.out.println("로그인에 실패하였습니다.\n");
+			
+		}
+		System.out.println(1. 회원정보);
+		System.out.println(2. 커뮤니티);
+		System.out.println(3. 쇼핑);
+		System.out.println(4. 로그아웃);
+
+	}
+
+}
+*/
+
+/*
+ class LoginService {
+	private static final String DB_ID = "admin";
+	private static final String DB_PW = "1234";
+	private static int count = 0;
+	
+	public static void login(User user) {
+		try {
+			if(!DB_ID.equals(user.getId())) {
+				
+			}
+		}
+*/
+	}
+} 

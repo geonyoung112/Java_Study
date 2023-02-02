ce971d3dfc76b733fd2b110abd0db2d7d6b434c6
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-02 11:44:27 +0900

    modify
    2023.02.02

--- work12/test/src/com/test/ch11/Ex04Country.java
+++ work12/test/src/com/test/ch11/Ex04Country.java
@@ -6,11 +6,11 @@ public enum Ex04Country {
 	UK(44, 1.3f),
 	ETC(0, 1.5f);
 
-	private final int code;
-	private final float tax;
+	private final int code; //고정된 값이 바뀌면 안되기 때문에 final로 생성
+	private final float tax;  //고정된 값이 바뀌면 안되기 때문에 final로 생성
 	
 	Ex04Country(int code, float tax) {
-		this.code = code;
+		this.code = code; 
 		this.tax = tax;
 	}
 	

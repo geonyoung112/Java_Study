f2c9204385404abe38e2325ef60d61e50464ada4
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-06 11:24:06 +0900

    modify
    2023.02.06

--- work12/test/src/com/test/ch12/Ex01TryCatch.java
+++ work12/test/src/com/test/ch12/Ex01TryCatch.java
@@ -7,11 +7,13 @@ public class Ex01TryCatch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//예외처리
+		//개발자가 개발하면서 만드는 오류가 아닌 사용자가 입력하거나 사용하면서 생기는 오류를 예외로 처리하며 
+		//눈에 띄게 하는 것
 		Scanner scanner = new Scanner(System.in);
 		
 		try {
 			System.out.print("문자열을 일력하여 예외를 발생시키세요: ");
-			int num = scanner.nextInt();
+			int num = scanner.nextInt(); // 여기서 잘못 입력되면 캐치문으로 넘어간다
 			System.out.println(num);
 			
 		}catch(Exception e) {

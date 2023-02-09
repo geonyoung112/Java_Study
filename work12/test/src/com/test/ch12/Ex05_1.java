6743821e20456d0a34da49c1ae5eb58d0ff761f9
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-09 10:35:30 +0900

    Update
    2023.02.09
    vending machine

--- work12/test/src/com/test/ch12/Ex05_1.java
+++ work12/test/src/com/test/ch12/Ex05_1.java
@@ -1,5 +1,6 @@
 package com.test.ch12;
 
+import java.util.Scanner;
 
 //자판기 프로그램
 //VendingMachine
@@ -7,6 +8,9 @@ public class Ex05_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Vendingmachine.User();
+		
+		
 	
 
 	}
@@ -18,8 +22,7 @@ class Vendingmachine {
 	
 	static String[] box = new String[Max];
 	static int[] price = new int[Max];
-	static int count = 4;
-	static int admin = 1004;
+	static int count = 5;
 	
 	
 	
@@ -43,30 +46,77 @@ class Vendingmachine {
 	}
 	
 	public static void User() {
+		Scanner scanner = new Scanner(System.in);
 		Boolean run = true;
 		int money = 0;
 	
 		while(true) {
-			System.out.println("---음료 자판기---");
-			for(int i=0; i<count; i++) {
-				System.out.println("메뉴: " + (i+1)+ ": " + box[i] + ", " + price[i] + "원 입니다.");
+			
+			try {
+				list();
+				
+				System.out.println("---음료 자판기---");
+					for(int i=0; i<count; i++) {
+					System.out.println("메뉴" + (i+1)+ ": " + box[i] + ", " + price[i] + "원");
+					
+					}
+					System.out.println("--------------");
+					
+					if(run) {
+					System.out.println("동전이나 지폐를 넣어주세요: ");
+					money = scanner.nextInt();
+				
+					System.out.println("메뉴를 골라두세요: ");
+					int num = scanner.nextInt();
+				
+					if(money >= price[num-1]) {
+						System.out.println(box[num-1] + " 이/가 나왔습니다.");
+						money = money - price[num-1];
+						System.out.println("잔액은 " + money + "입니다.");
+						System.out.println();
+						
+					} else {
+						System.out.println("현재 잔액이 부족합니다.");
+						System.out.println("고객님의 " + money +"원을 반환합니다.");
+						System.out.println();
+					}
+					
+				}
+				
+			} catch(Exception e){
+				System.out.println("메뉴를 다시 선택해주세요.");
+				continue;
+				
 			}
+				
+			if(money == 0) {
+				System.out.println("이용해주셔서 감사랍니다.");
+				run = true;
+			}
+			
+			
+			
+			System.out.println("1: 계속 구매하기 2: 잔돈 반환하기" );
+			System.out.println("번호를 입력하세요: ");
+			int num1 = scanner.nextInt();
+			try {
+				
+				if(num1 == 1) {
+					continue;
+				} 
+				else if (num1 == 2) {
+					System.out.println("거스름돈 " + money + "원이 반환됩니다.");
+					System.out.println("감사합니다.");
+					break;
+					
+				}
+			} catch(Exception e) {
+				System.out.println("보기에 나온 숫자를 입력해주세요.");
+			}
+			
 		}
 	}
 }
-
-
-
-/*class User{
-	private Boolean run = true;
-	private int money = 0;
-	
-	while(true) {
-		System.out.println("----음료 자판기-----");
-		
-	}
-}
-*/
 
 /*
 문제 1.

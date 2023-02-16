868458cacec213a969c03626c948c4db296e2101
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-16 10:55:39 +0900

    Create
    문제 풀기

--- work12/test/src/com/test/ch14/Ex01_1.java
+++ work12/test/src/com/test/ch14/Ex01_1.java
@@ -4,7 +4,33 @@ public class Ex01_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//1. ==
+		String str1 = "가나다라";
+		String str2 = new String("가나다라");
+		
+		if(str1 == str2) {
+			System.out.println("두개의 값이 같습니다.");
+		}else{
+			System.out.println("두개의 값이 같지 않습니다.");
+		}
+		
+		//2. equals
+		String s1 = "abcd";
+		String s2 = new String("abcd");
+		
+		if(s1.equals(s2)) {
+			System.out.println("두개의 값이 같습니다.");
+		}else {
+			System.out.println("두개의 값이 같지 않습니다.");
+		}
+		
 
 	}
 
 }
+
+/*
+문제 1.
+== 과 equals 의 차이를 설명할 수 있는 예제를 작성하세요.
+*/
---
+++ work12/test/src/com/test/ch14/Ex02_1.java
@@ -0,0 +1,45 @@
+package com.test.ch14;
+
+public class Ex02_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "안녕하세요, 엘컴#퓨터@@학원 입니다.!~%";
+		String filter = ",@!~%#";
+		String result = "";
+		
+		String target = " ";
+		int pos = 0;
+		int count = 0;
+		
+		//1. 특정 문자열 제거
+		for(int i=0; i<s1.length(); i++) {
+			char ch = s1.charAt(i);
+			
+			if(filter.indexOf(ch) == -1) {
+				result += String.valueOf(ch);
+			}
+		}
+		
+		System.out.println(result);
+		
+		//2. 공백의 수 
+		//1의 결과값을 이용하고 타겟으로 공백을 설정 후 연산자 우선수위로 인해 () 작성하기
+		while((pos = result.indexOf(target, pos)) != -1) { 
+			pos += target.length();
+			count++;
+			
+		}
+		System.out.println("공백의 수: " + count);
+
+	}
+
+}
+
+/*
+문제 1.
+"안녕하세요, 엘컴#퓨터@@학원 입니다.!~%"
+위 문자열에서 ,@!~%# 문자를 제거한 뒤 출력하는 프로그램을 코딩하세요.
+문제 2.
+위 문자열에서 공백의 수를 카운팅하여 출력하세요.
+*/

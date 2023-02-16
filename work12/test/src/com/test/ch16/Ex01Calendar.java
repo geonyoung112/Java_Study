b7db19dca4646feaf288448634c75acc89081b2e
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-16 10:29:50 +0900

    Create
    예제 작성

--- work12/test/src/com/test/ch16/Ex01Calendar.java
+++ work12/test/src/com/test/ch16/Ex01Calendar.java
@@ -11,7 +11,7 @@ public class Ex01Calendar {
 		int year = now.get(Calendar.YEAR);
 		System.out.println(year);
 		
-		int month = now.get(Calendar.MONTH) +1;
+		int month = now.get(Calendar.MONTH) +1; 	// 0 ~ 11
 		System.out.println(month);
 		
 		int day = now.get(Calendar.DATE);
@@ -32,8 +32,63 @@ public class Ex01Calendar {
 		int millisecond = now.get(Calendar.MILLISECOND);
 		System.out.println(millisecond);
 		
-	
-
+		int ampm = now.get(Calendar.AM_PM);
+		String strAmpm = ampm == 0 ? "AM" : "PM";
+		System.out.println(ampm + ", " + strAmpm);	 // 0: am, 1: pm
+		
+		String[] strDayOfWeek = {"", "일", "월", "화", "수", "목", "금", "토"};
+		int dayOfWeek = now.get(Calendar.DAY_OF_WEEK);	 // 1:일, 2:월, 3:화, 4:수, 5:목, 6:금, 7:토
+		System.out.println(dayOfWeek + ", " + strDayOfWeek[dayOfWeek]);
+		
+		String strDateTime = year + "-" + month + "-" + day + " " + hour12 + ":" + minute + ":" + second + "." + millisecond 
+				+ " " + strAmpm + " " + strDayOfWeek[dayOfWeek] + "오일";
+		
+		System.out.println(strDateTime);
+		System.out.println();
+		
+		Calendar start = Calendar.getInstance();
+		Calendar end = Calendar.getInstance();
+		end.set(2030, 9, 7, 0, 0, 0);
+		System.out.println(start.getTime());
+		System.out.println(end.getTime());
+		
+		long diffMilli = end.getTimeInMillis() - start.getTimeInMillis();		// 밀리세컨즈
+		System.out.println(diffMilli + " 밀리초");
+		long diffSecond = diffMilli/(1000);		// 초
+		System.out.println(diffSecond + " 초");
+		long diffMinute = diffMilli / (60 * 1000);	// 분
+		System.out.println(diffMinute + " 분");
+		long diffHour = diffMilli / (60 * 60 * 1000);	// 시간
+		System.out.println(diffHour + " 시간");
+		long diffDay = diffMilli / (24 * 60 * 60 * 1000);	// 일
+		System.out.println(diffDay + " 일");
+		long diffYear = diffMilli / (365L * 24 * 60 * 60 * 1000);	// 년
+		System.out.println(diffYear + " 년");
+		System.out.println();
+		
+		System.out.println(diffDay / 365 + "년 ");
+		System.out.println(diffDay % 365 + " 일 차이");
+		System.out.println();
+		
+		Calendar today = Calendar.getInstance();
+		System.out.println(today.getTime().toString());
+		
+		today.add(Calendar.DATE, 1);
+		System.out.println(today.getTime().toString());
+		
+		today.add(Calendar.DATE, -1);
+		System.out.println(today.getTime().toString());
+		
+		today.set(Calendar.DATE, 22);
+		System.out.println(today.getTime().toString());
+				
 	}
 
 }
+
+/*
+문제 1.
+2000년 12월 1일 
+2020년 5월 6일 
+두 날짜 간의 차이를 계산하여 n년 n일과 같은 패턴으로 출력하세요. 
+*/

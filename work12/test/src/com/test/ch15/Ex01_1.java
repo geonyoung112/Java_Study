1ddda8bfa277ecb893f116f48d13f23f51201e22
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-17 11:07:42 +0900

    Create
    예제 작성

--- work12/test/src/com/test/ch15/Ex01_1.java
+++ work12/test/src/com/test/ch15/Ex01_1.java
@@ -8,3 +8,18 @@ public class Ex01_1 {
 	}
 
 }
+/*
+문제 1.
+int a = 3;
+int b = 7;
+Integer intgA;
+Integer intgB;
+a와 b의 값을 갖는 intgA, intgB를 코딩하세요.
+문제 2.
+intgA와 intgB의 값을 더한 뒤 int로 변환하여 출력하세요.
+문제 3.
+intgA와 intgB의 값을 compareTo를 이용하여 비교하고 더 큰 값을 출력하세요.
+문제 4.
+intgA 와 intgB의 최소값과 최대값을 구하세요.
+힌트) max, min을 사용하세요.
+*/

e53586880983993ed1c0a51a8fa81d8bb42152a1
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-21 09:51:56 +0900

    Create
    문제 풀이

---
+++ work12/test/src/com/test/ch16/Ex01_1.java
@@ -0,0 +1,32 @@
+package com.test.ch16;
+
+import java.util.Calendar;
+
+public class Ex01_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calendar start = Calendar.getInstance();
+		start.set(2000, 12, 1, 0, 0, 0);
+		Calendar end = Calendar.getInstance();
+		end.set(2030, 9, 7, 0, 0, 0);
+		
+		System.out.println(start.getTime());
+		System.out.println(end.getTime());
+		System.out.println();
+		
+		
+		long diffMilli = end.getTimeInMillis() - start.getTimeInMillis();		// 밀리세컨즈
+		long diffDay = diffMilli / (24 * 60 * 60 * 1000);	// 일
+		
+		System.out.println("두 날짜 간의 차이: " + diffDay / 365 + "년 "  + diffDay % 365 + " 일 차이");
+		
+	}
+
+}
+/*
+문제 1.
+2000년 12월 1일 
+2020년 5월 6일 
+두 날짜 간의 차이를 계산하여 n년 n일과 같은 패턴으로 출력하세요. 
+*/

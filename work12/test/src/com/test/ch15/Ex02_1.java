ed098b61024ba739fc8ae14765c1df499ea4f555
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-21 09:24:18 +0900

    Create
    문제 풀기

---
+++ work12/test/src/com/test/ch15/Ex02_1.java
@@ -0,0 +1,27 @@
+package com.test.ch15;
+
+public class Ex02_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer intgA = Integer.valueOf(3);
+		System.out.println(intgA.intValue());
+		
+		System.out.println("+");
+		
+		Integer intgB = Integer.valueOf(7);
+		System.out.println(intgB.intValue());
+		
+		
+		Integer intgC = Integer.valueOf(intgA.intValue() + intgB.intValue());
+		System.out.println("= " + intgC.intValue());
+	}
+
+}
+/*
+문제 1.
+Integer intgA = 3;
+Integer intgB = 7;
+int sum = intgA + intgB;
+위 내용을 오토박싱과 오토언박싱이 필요 없는 코드로 수정하세요.
+*/

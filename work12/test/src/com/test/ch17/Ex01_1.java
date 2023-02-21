614199a01da85aa5d45683c3208e3d3bd2499d0a
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-21 11:48:36 +0900

    Create
    문제 풀이

---
+++ work12/test/src/com/test/ch17/Ex01_1.java
@@ -0,0 +1,20 @@
+package com.test.ch17;
+
+public class Ex01_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/*
+문제 1.
+Book 타입 인스턴스 10개를 생성하여 배열에 저장합니다.
+배열을 ArrayList로 변경합니다.
+foreach 문을 사용하여 리스트에 저장된 모든 책들을 출력합니다.
+Book의 저자가 엘컴퓨터학원인 것을 제외한 나머지 책은 모두 삭제합니다.
+리스트에 남아 있는 책들을 출력합니다.
+힌트) 삭제 시 foreach문은 예외가 발생될 수 있습니다.
+*/

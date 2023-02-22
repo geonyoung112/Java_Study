0b7c0677b9a255e4c1e202ecc57dcf1bce3e1d55
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-02-22 11:17:10 +0900

    Update
    주석작성

--- work12/test/src/com/test/ch17/Ex01_1.java
+++ work12/test/src/com/test/ch17/Ex01_1.java
@@ -1,14 +1,92 @@
 package com.test.ch17;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.List;
+
 public class Ex01_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Book1 b1 = new Book1("자바1", "엘컴퓨터학원");
+		Book1 b2 = new Book1("자바2", "엘컴퓨터학원");
+		Book1 b3 = new Book1("자바3", "엘컴퓨터학원");
+		Book1 b4 = new Book1("자바4", "엘컴퓨터학원");
+		Book1[] arrBook = new Book1[10];
+		List<Book1> books2 = new ArrayList<Book1>(Arrays.asList(arrBook));
+		System.out.println(books2.toString());
+
+			
 
 	}
 
 }
 
+class Book1 {
+	private String title;
+	private String author;
+	
+	public Book1(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	@Override 
+	public String toString() {
+		return "Book[title=" + title + ", author=" + author + "]";
+	}
+}
+
+class Cart1 {
+	private List<Book1> books1;
+	
+	public Cart1() {
+		books1 = new ArrayList<Book1>(10);
+	}
+	public void add(Book1 book) {
+		books1.add(book);
+	}
+	public void add(int index, Book1 book) {
+		books1.add(index, book);
+	}
+	public boolean checkForDuplicateBook(Book1 book) {
+		return books1.contains(book);
+	}
+	public List<Book1> getAllBooks() {
+		return books1;
+	}
+	public Book1 getBook(int index) {
+		return books1.get(index);
+	}
+	
+	public static void printAllBooksWithForEach(List<Book1> books1) {
+		System.out.println("foreach");
+		for (Book1 book : books1) {		// 바이트 코드 변환 시 보일러 플레이트 최소화
+			System.out.println(book);
+		}
+	}
+	
+	public static void removeWithForEach(List<Book1> books1) { 
+		try {
+			for (Book1 book : books1) {		// Iterable
+				books1.remove(book);
+				System.out.println("삭제: " + book);
+			}
+		} catch (ConcurrentModificationException e) {
+			System.out.println("Iterable은 삭제 기능을 지원하지 않습니다.\n");
+		}
+		
+		System.out.println(books1.toString());
+	}
+	
+}
 /*
 문제 1.
 Book 타입 인스턴스 10개를 생성하여 배열에 저장합니다.

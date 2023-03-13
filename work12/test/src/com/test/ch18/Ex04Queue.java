7cc22b92a3d234b4ae6b7d10abd30ac0956a7b73
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-03-13 10:26:07 +0900

    Update
    예제작성, 문제풀이

--- work12/test/src/com/test/ch18/Ex04Queue.java
+++ work12/test/src/com/test/ch18/Ex04Queue.java
@@ -5,7 +5,14 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Ex04Queue {
-
+	/*
+	 * 큐의 특징
+	 * 1. 먼저 들어간 자료가 먼저 나오는 구조(FIFO)
+	 * 2. 큐는 한 쪽 끝은 프런트로 정하여 삭제 연산만 수행
+	 * 3. 다른 한 쪽 끝은 리어로 정하여 삽입 연산만 수행
+	 * 4. 그래프의 넓이 우선 탐색에서 사용
+	 * 5. 컴퓨터 버퍼에서 주로 사용, 마구 입력이 되었으나 처리를 하지 못할 때, 버퍼(큐)를 만들어 대기 시킴
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		QueueTicketDispenser.takeNum();
@@ -35,15 +42,19 @@ class QueueTicketDispenser {
 	
 	public static int takeNum() {
 		Restaurant.q.offer(num);
+		//큐에 값을 추가하고 싶다면 add(value), offer(value)를 쓰기
 		return num++;
 	}
 }
 
 class Restaurant {
 	public static Queue<Integer> q = new LinkedList<>();
+	//자바의 큐는 링크드리스트를 활용하기
+	//Queue<Element> queue = new LinkedList<>()와 같이 선언
 	
 	public static void seving() {
 		int num = q.poll();
+		//큐에서 첫번째 값을 반환하고 제거, 비어있다면 null
 		System.out.println(num + "번 손님에게 서빙되었습니다.");
 	}
 	
@@ -59,6 +70,7 @@ class Restaurant {
 	
 	public static int firstGuest() {
 		return q.peek();
+		// 큐에서 첫번째로 저장된 값을 참조하고 싶을 때 사용
 	}
 }
 
--- work12/test/src/com/test/ch18/Ex04_1.java
+++ work12/test/src/com/test/ch18/Ex04_1.java
@@ -1,15 +1,104 @@
 package com.test.ch18;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Ex04_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		Queue<String> notices = new LinkedList<>();
+		
+		while(true) {
+			System.out.println("- 공지사항 -");
+			System.out.println("1. 공지사항 등록");
+			System.out.println("2. 공지사항 목록");
+			System.out.println("3. 공지사항 삭제");
+			System.out.println("4. 종료");
+			
+			int choice = scanner.nextInt();
+			scanner.nextLine();
+			
+			Function[] functions = Function.values();
+			Function menu = functions[choice-1];
+			
+			
+			switch(menu) {
+				case ADD:
+					System.out.println("제목과 내용을 입력하세요: ");
+					String notice = scanner.nextLine();
+					notices.offer(notice);
+					break;
+				case LIST:
+					if (notices.isEmpty()) {
+						System.out.println("공지사항이 없습니다.");
+					} else {
+						System.out.println("- 공지사항 목록 -");
+						for (String num : notices) {
+							System.out.println("- " + num + " -");
+						}
+					}
+					break;
+				case DELETE:
+					if(notices.isEmpty()) {
+						System.out.println("공지사항이 없습니다.");
+					} else {
+						String oldestNotice = notices.poll();
+						System.out.println("오래된 순으로 삭제하겠습니다.");
+						System.out.println("[" + oldestNotice + "] 가 삭제되었습니다.");
+					}
+					break;
+				case EXIT:
+					System.out.println("프로그램을 종료합니다.");
+					System.exit(0);
+				default:
+					System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+			}
+		}
 	}
-
 }
-class notice {
+
+
+class Notice {
+	private int id;
+	private String title;
+	private String content;
 	
+	
+	public Notice(int id, String title, String content) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getContent() {
+		return content;
+	}
+}
+
+enum Function{
+    ADD(1),
+    LIST(2),
+    DELETE(3),
+    EXIT(4);
+	
+
+    private final int value;
+
+    Function(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
 /*
 문제 1.

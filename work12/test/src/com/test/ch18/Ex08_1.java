5116deca275a1d29b23b07a299bbbe6cf8aeea29
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-03-14 11:51:15 +0900

    Update
    문제 풀이

--- work12/test/src/com/test/ch18/Ex08_1.java
+++ work12/test/src/com/test/ch18/Ex08_1.java
@@ -1,10 +1,45 @@
 package com.test.ch18;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Ex08_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		Deque<String> messageinput = new LinkedList<>();
+		
+		while(true) {
+			System.out.println("메세지를 입력하세요: ");
+			String message = scanner.nextLine();
+			
+			if(message.equals("quit")) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+				//equals 이용하기
+			}
+			messageinput.add(message);
+			
+			if(messageinput.size() > 3) {
+				messageinput.removeFirst();
+				//3보다 커지면 먼저 입력된 값들을 지운다
+				//quit를 입력하면 종료되기 때문에 구현을 위해 break을 입력하지 않는다.
+			}
+		}
+		System.out.println("마지막으로 입력된 3개의 메세지를 출력합니다.");
+		for(String message: messageinput) {
+			System.out.println(message);
+		}
+	
 	}
 
 }
+
+/*
+문제 1.
+반복문을 이용해 사용자가 입력한 메세지들을 저장하세요. 
+마지막으로 입력한 메세지 세개만 화면에 출력되도록 코딩하세요.
+quit가 입력되면 반복문은 종료되어야 합니다.
+*/

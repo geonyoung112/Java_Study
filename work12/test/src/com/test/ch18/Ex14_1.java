cc01a3ec7f82e2acdd6ed88b4fd41ab069f61e2e
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-03-20 10:32:34 +0900

    Update
    문제 풀이

--- work12/test/src/com/test/ch18/Ex14_1.java
+++ work12/test/src/com/test/ch18/Ex14_1.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Ex14_1 {
 	public static void main(String[] args) {
 		List<Student4> stu = new ArrayList<>();
 		Collections.addAll(stu, new Student4(2, "홍길동"), new Student4(1, "김갑"), new Student4(2,"장김"));
 		
+		//문제 1.
 		System.out.println("< 번호 내림차순, 이름 오름차순으로 정력>");
 		Collections.sort(stu);
 		System.out.println(stu);
 		System.out.println();
 		
+		//문제 2.
 		System.out.println("< 번호 내림차순 정렬, 이름 내림차순 정렬 >");
 		Collections.sort(stu, new Comparator<Student4>() {
 			@Override
@@ -27,12 +30,72 @@ public class Ex14_1 {
 		});
 		System.out.println(stu);
 		System.out.println();
-	}
+		
+		//문제 3.
+		// Comparable을 이용, 가격이 높은 책부터 제거
+        PriorityQueue<Book5> queue1 = new PriorityQueue<>();
+        queue1.add(new Book5(15000, "Java"));
+        queue1.add(new Book5(5000, "Python"));
+        queue1.add(new Book5(20000, "C++"));
+        
+        System.out.println("< Comparable을 이용해 가격이 높은 책부터 제거 >");
+        while (!queue1.isEmpty()) {
+            System.out.println(queue1.poll());
+        }
+        System.out.println();
+        
+        // Comparator를 이용해 가격이 낮은 책부터 제거
+        PriorityQueue<Book5> queue2 = new PriorityQueue<>(new DescendingPrice2());
+        queue2.add(new Book5(10000, "Java"));
+        queue2.add(new Book5(5000, "Python"));
+        queue2.add(new Book5(20000, "C++"));
+        
+        System.out.println("< Comparator를 이용해 가격이 낮은 책부터 제거 >");
+        while (!queue2.isEmpty()) {
+            System.out.println(queue2.poll());
+        }
+    }
 }
 
 
+//문제 3.
+class Book5 implements Comparable<Book5> {
+    private int price;
+    private String title;
+    
+    public Book5(int price, String title) {
+        this.price = price;
+        this.title = title;
+    }
+    
+    public int getPrice() {
+        return price;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    @Override
+    public int compareTo(Book5 b) {
+        return Integer.compare(b.getPrice(), this.price);
+    }
+    
+    @Override
+	public String toString() {
+		return "(" + price + ", " + title + ")";
+	}
+}
 
+//낮은 가격부터 제거 & 출력
+class DescendingPrice2 implements Comparator<Book5> {
+    @Override
+    public int compare(Book5 b1, Book5 b2) {
+        return Integer.compare(b1.getPrice(), b2.getPrice());
+    }
+}
 
+//문제 1, 2
 class Student4 implements Comparable<Student4> {
 	private int no;
 	private String name;
---
+++ work12/test/src/com/test/ch19/Ex01_1.java
@@ -0,0 +1,26 @@
+package com.test.ch19;
+
+public class Ex01_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+/*
+문제 1.
+BiPredicate
+BinaryOperator
+IntConsumer
+IntFunction
+IntPredicate
+IntUnaryOperator
+IntBinaryOperator
+IntToDoubleFunction
+ToIntFunction
+ToIntBiFunction
+위 함수형 인터페이스들을 이용해 람다식을 작성하고 출력하세요. 
+*/

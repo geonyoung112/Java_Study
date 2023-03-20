9eebc0e84eaaca10519b5428894d1f40856b35f4
geonyoung112 <76231792+geonyoung112@users.noreply.github.com>
2023-03-20 11:59:14 +0900

    Update

--- work12/test/src/com/test/ch19/Ex02_1.java
+++ work12/test/src/com/test/ch19/Ex02_1.java
@@ -1,5 +1,34 @@
 package com.test.ch19;
 
-public class Ex02_1 {
 
+public class Ex02_1 {
+	public static void main(String[] args) {
+		
+	}
 }
+
+
+
+class User2 {
+	private String number;
+	private int time;
+	public User2(String number, int time) {
+		this.number = number;
+		this.time = time;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public int getTime() {
+		return time;
+	}
+	@Override
+	public String toString() {
+		return "number=" + number + ", time=" + time;
+	}	
+}
+/*
+문제 2.
+차량 출입 시간이 5시에서 9시 사이인 차량만 출력하는 프로그램을 코딩하세요.
+위 회원 검색 프로그램과 같이 함수형 인터페이스를 작성하고 람다식을 사용해야 합니다.  
+*/
